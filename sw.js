ff004d62ea3c50b6f2f4cac8a5374912b12c3ee4
hadasGit <hadasbarnes76@gmail.com>
2025-12-02 11:42:37 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("app-cache").then((cache) => cache.addAll(["/","/index.html","/manifest.json"]))
+  );
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
+});
