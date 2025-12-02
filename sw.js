bff37a8168c21738c1bde79b4ea0946bcb6555ec
hadasGit <hadasbarnes76@gmail.com>
2025-12-02 11:51:02 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("app-cache").then((cache) => cache.addAll(["/","/index.html","/manifest.json"]))
-  );
-});
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((r) => r || fetch(event.request)));
-});
